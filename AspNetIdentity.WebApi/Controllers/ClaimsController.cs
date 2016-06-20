f1e7df230ef94cc5a7ce1aae618d8aa19d3960b8
geetha19983b <geethainfy@gmaiil.com>
2016-06-20 15:56:41 +0530

    commit

---
+++ AspNetIdentity.WebApi/Controllers/ClaimsController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Threading;
+using System.Web.Http;
+
+namespace AspNetIdentity.WebApi.Controllers
+{
+    [RoutePrefix("api/claims")]
+    public class ClaimsController : BaseApiController
+    {
+        [Authorize]
+        [Route("")]
+        public IHttpActionResult GetClaims()
+        {
+            var identity = User.Identity as ClaimsIdentity;
+            
+            var claims = from c in identity.Claims
+                         select new
+                         {
+                             subject = c.Subject.Name,
+                             type = c.Type,
+                             value = c.Value
+                         };
+            //var identity1 = (ClaimsPrincipal)Thread.CurrentPrincipal;
+            //var claims1 = identity1.Claims;
+            return Ok(claims);
+        }
+
+    }
+}
